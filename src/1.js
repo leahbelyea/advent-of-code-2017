e45cb8f53ec9f2dcb30c45981e1e00ce43f774e0
Belyea, Leah <lbelyea@cvent.com>
2017-12-02 05:58:21 -0500

    day 1

--- src/1.js
+++ src/1.js
@@ -1,15 +1,45 @@
-// Run with `npm run aoc 1`
-
 const _ = require('lodash');
-const {getInputString, getInputArray} = require('./helpers')
+const {getInputString} = require('./helpers')
 
 let input;
-
 input = getInputString('1.txt');
-console.log('Input string');
-console.log(input);
-console.log('\n');
 
-input = getInputArray('1.txt')
-console.log('Input array');
-console.log(input);
+// // Sample input
+// // Part 1
+// input = '1122'; // 3
+// input = '1111'; // 4
+// input = '1234'; // 0
+// input = '91212129'; // 9
+//
+// // Part 2
+// input = '1212'; // 6
+// input = '1221'; // 0
+// input = '123425'; // 4
+// input = '123123'; // 12
+// input = '12131415'; // 4
+
+input = input.split('');
+
+// Part 1
+let doubleNums = [];
+_.each(input, (char, index) => {
+  const nextChar = _.get(input, `[${(index + 1) % _.size(input)}]`);
+  if (char === nextChar) {
+    doubleNums.push(parseInt(char));
+  }
+});
+
+console.log('# Part 1 #');
+console.log(_.sum(doubleNums));
+
+// Part 2
+doubleNums = [];
+_.each(input, (char, index) => {
+  const nextChar = _.get(input, `[${(index + (_.size(input) / 2)) % _.size(input)}]`);
+  if (char === nextChar) {
+    doubleNums.push(parseInt(char));
+  }
+});
+
+console.log('\n# Part 2 #');
+console.log(_.sum(doubleNums));
